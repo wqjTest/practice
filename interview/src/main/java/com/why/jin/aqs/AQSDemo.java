5e7c80ee8111d3597c65473587c1448381f926a9
wqj <qianjin.wang@atongmu.net>
2021-06-23 16:46:27 +0800

    master 2021-06-23 16:45 面试题

--- interview/src/main/java/com/why/jin/aqs/AQSDemo.java
+++ interview/src/main/java/com/why/jin/aqs/AQSDemo.java
@@ -1,4 +1,4 @@
-package com.why.jin.redisLock;
+package com.why.jin.aqs;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
@@ -17,7 +17,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class AQSDemo {
 
     public static void main(String[] args) {
-        ReentrantLock lock = new ReentrantLock();
+        ReentrantLock lock = new ReentrantLock(true);
         //带入一个银行办理业务的案例来模拟我们的AQS如何进行线程的管理和通知唤醒机制
 
         //3个线程模拟3个来银行网点，受理窗户办理业务的顾客
