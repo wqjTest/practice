1b01577079aa947bd031a365877425e1ac4c58bc
wqjTest <991575842@qq.com>
2022-07-03 20:42:05 +0800

    链表

--- dataStructure-algorithm/src/main/java/com/why/jin/dataStructure/linkedList/SingleLinkedListDemo.java
+++ dataStructure-algorithm/src/main/java/com/why/jin/dataStructure/linkedList/SingleLinkedListDemo.java
@@ -7,42 +7,51 @@ package com.why.jin.dataStructure.linkedList;
 public class SingleLinkedListDemo {
 
     public static void main(String[] args) {
-          //进行测试
-          //先创建节点
-          HeroNode hero1 = new HeroNode(1,"宋江","及时雨");
-          HeroNode hero2 = new HeroNode(2,"卢俊义","玉麒麟");
-          HeroNode hero3 = new HeroNode(3,"吴用","智多星");
-          HeroNode hero4 = new HeroNode(4,"林冲","豹子头");
-          //创建要给链表
-          SingleLinkedList singleLinkedList = new SingleLinkedList();
-          //加入
-          singleLinkedList.add(hero1);
-          singleLinkedList.add(hero2);
-          singleLinkedList.add(hero3);
-          singleLinkedList.add(hero4);
-          //显示一把
-          singleLinkedList.list();
+        //进行测试
+        //先创建节点
+        HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
+        HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
+        HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
+        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
+        //创建要给链表
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        //加入
+        /*singleLinkedList.add(hero1);
+        singleLinkedList.add(hero2);
+        singleLinkedList.add(hero3);
+        singleLinkedList.add(hero4);*/
+        // 加入按照编号顺序
+        singleLinkedList.addByOrder(hero1);
+        singleLinkedList.addByOrder(hero4);
+        singleLinkedList.addByOrder(hero3);
+        singleLinkedList.addByOrder(hero2);
+
+        //测试修改节点的代码
+        HeroNode newHeroNode = new HeroNode(2,"小卢","玉麒麟~~");
+        singleLinkedList.update(newHeroNode);
+        //显示一把
+        singleLinkedList.list();
     }
 
 }
 
 //定义SingleLinkedList 管理我们的英雄
-class SingleLinkedList{
+class SingleLinkedList {
     //先初始化一个头结点，头结点不要动
-    private HeroNode head = new HeroNode(0,"","");
+    private HeroNode head = new HeroNode(0, "", "");
 
     //添加节点到单向链表
     //思路，当无考虑编号顺序是
     //1.找到当前链表的最后节点
     //2.将最后这个节点的next 指向 新的节点
-    public void add(HeroNode heroNode){
+    public void add(HeroNode heroNode) {
 
         //因为head节点不能动，因此我们需要一个辅助遍历temp
         HeroNode temp = head;
         //遍历链表，找到最后
-        while (true){
+        while (true) {
             //找到链表的最后
-            if (temp.next == null){
+            if (temp.next == null) {
                 break;
             }
             //如果没有找到最后，将temp后移
@@ -53,18 +62,80 @@ class SingleLinkedList{
         temp.next = heroNode;
     }
 
+    //第二种方式在添加英雄时，根据排名将英雄插入指定位置
+    //(如果有这个排名，则添加失败，并给出提示)
+    public void addByOrder(HeroNode heroNode) {
+        //因为头节点不能动，因此我们任然通过一个辅助指针（变量）来帮助找到添加的位置
+        //因为单链表，因为我们找的temp 是位于 添加位置的前一个节点，否则插入不了
+        HeroNode temp = head;
+        boolean flag = false; //flag标志添加的编号是否存在，默认为false
+        while (true) {
+            if (temp.next == null) { //说明temp已经在链表的最后
+                break;
+            }
+            if (temp.next.no > heroNode.no) { //说明希望添加的heroNode的编号已然存在
+                break;
+            } else if (temp.next.no == heroNode.no) { //说明希望添加的heroNode的编号已然存在
+                flag = true; // 说明编号存在
+                break;
+            }
+            temp = temp.next;//后移，遍历当前链表
+        }
+        //判断flag的值
+        if (flag) {//不能添加，说明编号存在
+            System.out.printf("准备插入的英雄的编号%d已经存在了，不能加入\n", heroNode.no);
+        } else {
+            //插入到链表中，temp的后面
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+    }
+
+    //修改节点的信息，根据no编号来修改，即no编号不能改
+    //说明
+    //1、根据newHeroNode  的 no 来修改即可
+    public void update(HeroNode newHeroNode) {
+        //判断是否空
+        if (head.next == null) {
+            System.out.println("链表为空~");
+            return;
+        }
+        //找到需要修改的节点，根据no编号
+        //定义一个辅助变量
+        HeroNode temp = head.next;
+        boolean flag = false;//表示是否找到该节点
+        while (true) {
+            if (temp == null) {
+                break;//已经遍历完链表
+            }
+            if (temp.no == newHeroNode.no){
+                //找到
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //根据flag判断是否找到要修改的节点
+        if (flag){
+            temp.name = newHeroNode.name;
+            temp.nickName = newHeroNode.nickName;
+        }else {//没有找到
+            System.out.printf("没有找到编号%d的节点，不能修改\n",newHeroNode.no);
+        }
+    }
+
     //显示链表【遍历】
-    public void list(){
+    public void list() {
         //判断链表是否为空
-        if (head.next == null){
+        if (head.next == null) {
             System.out.println("链表为空");
             return;
         }
         //因为头节点，不能动，因此我们需要一个辅助变量来遍历
         HeroNode temp = head.next;
-        while (true){
+        while (true) {
             //判断是否到链表最后
-            if (temp == null){
+            if (temp == null) {
                 break;
             }
             //输出节点信息
@@ -77,11 +148,12 @@ class SingleLinkedList{
 
 
 //定义HeroNode，每个HeroNode对象就是一个节点
-class HeroNode{
+class HeroNode {
     public int no;
     public String name;
     public String nickName;
     public HeroNode next;
+
     //构造器
     public HeroNode(int hNo, String hName, String hNickName) {
         this.no = hNo;
